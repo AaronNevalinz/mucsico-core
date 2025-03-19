87232ceba46e0f0ffef09e0d5b20e9ee20e41c94
AaronNevalinz <aaronug421@gmail.com>
2025-03-19 09:04:05 +0300

    initial project set up

---
+++ src/main/java/com/mucsico/MucsicoApplication.java
@@ -0,0 +1,13 @@
+package com.mucsico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MucsicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MucsicoApplication.class, args);
+	}
+
+}
